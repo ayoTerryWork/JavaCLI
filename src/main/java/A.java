4d6d2a1ad21941546af3a5b6a5d9d57c8c9a4695
ayoTerryWork <ayoterrywork@gmail.com>
2019-01-20 15:30:58 +0000

    code test locally working mysql.

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,38 +21,37 @@ import java.util.Scanner;
 class A {
     public static void main(String args[]) {
 
-
+        connectToDatabase();
         System.out.println("Welcome to Ayo's Polestar CLi in java");
         Scanner scanner = new Scanner(System.in);
 
-            try {//use for import statements
-                System.out.println("please input json data location (use forward slash '/' and start from drive letter)");
-                JSONParser parser = new JSONParser();
-                String jsonLocation =scanner.next();
-                Object jsonInput = parser.parse(new FileReader(jsonLocation));
-                JSONArray fromJson = (JSONArray) jsonInput;
-                int  arrayIndexAt =0;
-                do {
-                    JSONObject singleUnique = (JSONObject) fromJson.get(arrayIndexAt); //for each pulled json get their values and put it in the right place in the table.
-                    System.out.println(singleUnique);
-                    System.out.println(singleUnique.get("UniqueID"));
-                    //Get everthing not from parameters for insert statement here.
-                   JSONArray parameters = (JSONArray) singleUnique.get("Parameters");
-                   HashMap  individualParameters = getParameters(parameters);
-                    System.out.println(individualParameters.get("Name") + "Hashmaps!!");
-                    //Insert statements to take from the hashmap and the above ARRAY
+        try {//use for import statements
+            System.out.println("please input json data location (use forward slash '/' and start from drive letter)");
+            JSONParser parser = new JSONParser();
+            String jsonLocation = scanner.next();
+            Object jsonInput = parser.parse(new FileReader(jsonLocation));
+            JSONArray fromJson = (JSONArray) jsonInput;
+            int arrayIndexAt = 0;
+            do {
+                JSONObject singleUnique = (JSONObject) fromJson.get(arrayIndexAt); //for each pulled json get their values and put it in the right place in the table.
+                System.out.println(singleUnique);
+                System.out.println(singleUnique.get("UniqueID"));
+                //Get everthing not from parameters for insert statement here.
+                JSONArray parameters = (JSONArray) singleUnique.get("Parameters");
+                HashMap individualParameters = getParameters(parameters);
+                System.out.println(individualParameters.get("Name") + "Hashmaps!!");
+                //Insert statements to take from the hashmap and the above ARRAY
 
 
+                arrayIndexAt++;
 
-                    arrayIndexAt++;
-
-                } while (arrayIndexAt < fromJson.size());
+            } while (arrayIndexAt < fromJson.size());
 
 
 //                System.out.println(fromJson.get(1)+"What is this?");
-            } catch (ParseException | IOException e) {
-                e.printStackTrace();
-            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
 
 
         System.out.println("Please input JDBC String e.g. jdbc:mysql://localhost:3306/testdb?useSSL=false ");
@@ -60,20 +60,20 @@ class A {
         String url = input;
     }
 
-    public  static HashMap getParameters (JSONArray jsonparam){
-        HashMap <String, String> datapairs = new HashMap<>();
+    public static HashMap getParameters(JSONArray jsonparam) {
+        HashMap<String, String> datapairs = new HashMap<>();
         int parametersArray = 0;
-                    do {
-                        JSONObject aParameter = (JSONObject) jsonparam.get(parametersArray);
-                        datapairs.put((String)aParameter.get("Key"),(String) aParameter.get("Value"));
-                        System.out.println(aParameter + "A parameter");
-                        parametersArray++;
-                    }while (parametersArray < jsonparam.size() );
+        do {
+            JSONObject aParameter = (JSONObject) jsonparam.get(parametersArray);
+            datapairs.put((String) aParameter.get("Key"), (String) aParameter.get("Value"));
+            System.out.println(aParameter + "A parameter");
+            parametersArray++;
+        } while (parametersArray < jsonparam.size());
 
-        return  datapairs;
+        return datapairs;
     }
 
-    public static void createDatabase(){
+    public static void createDatabase() {
         try {
             //if given real database params:
             //use JDBC as if a real database if given a connection
@@ -81,13 +81,47 @@ class A {
             // Use database,
             //create table
             //else output params to file and system out println them
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
-    public static String createInserts(){
-    //To create the insert statements
 
-    return null;
+    public static String connectToDatabase() {
+        Connection connect = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/test?useSSL=false","root", "nataku"
+                     );
+            statement = connect.createStatement();
+            ResultSet queryTest= statement.executeQuery("select * from test.testdata1");
+            while (queryTest.next()){
+                System.out.println(queryTest.getString(1));
+            }
+
+//            PreparedStatement query = connect.prepareStatement("select * from test.testdata1");
+//           query.executeQuery();
+//            System.out.println(query);
+//            System.out.println(queryTest);
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Didnt work");
+        }
+        return "failure";
+    }
+
+    public static String createInserts() {
+        //To create the insert statements
+
+
+        return null;
+    }
+
+    public static String getContents(){
+        return  null;
     }
 }
